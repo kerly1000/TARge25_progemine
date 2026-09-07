56c53639caabc1638926e5767112fb5f22df2304
Lennart-2 <megalendur@gmail.com>
2026-09-07 15:45:09 +0300

    Spaceship link in menu

--- TARge25Shop/TARge25ShopData/TARge25ShopContext.cs
+++ TARge25Shop/TARge25ShopData/TARge25ShopContext.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+using Microsoft.EntityFrameworkCore;
 
 namespace TARge25Shop.Data
 {
-    internal class TARge25ShopContext
+    // nimetasime classi TARge25ShopContext, mis pärib DbContext klassi
+    public class TARge25ShopContext : DbContext
     {
+        // tegime konteksti, mis pärib DbContext klassi
+        public TARge25ShopContext(DbContextOptions<TARge25ShopContext> options)
+            : base(options) { }
+
+     
     }
 }
